ddb89b0533d5c484a874db9660318550f57cbbdb
pranjalbansal1903 <pranjalbansal66@gmail.com>
2024-08-24 16:50:00 +0530

    first commit

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,68 @@
+
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import {errorHandler} from '../utils/error.js'
+
+export const test  = (req,res) => {
+
+
+    res.json({
+        message: 'API route is working!',
+    });
+
+};
+
+export const updateUser = async(req,res,next)=>
+{
+if (req.user.id != req.params.id) return next(errorHandler(401, 'you can only update your own accoun!') );
+try{
+
+    if(req.body.password){
+
+        req.body.password = bcryptjs.hashSync(req.body.password,10)
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+
+      $set:{
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar,
+      }  
+    }, {new:true})
+
+    const{password, ...rest} = updatedUser._doc;
+    res.status(200).json(rest);
+
+    } catch(error)
+    {
+        next(error);
+    }
+};
+
+export const deleteUser = async(req,res,next) =>
+{
+    if (req.user.id !== req.params.id) return next (errorHandler(401,'you can only delete your own account'))
+try {
+
+await User.findByIdAndDelete(req.params.id)
+
+res.clearCookie('access_token');
+
+res.status(200).json('user has been deleted successfully')
+
+
+
+
+}
+
+catch(error){
+next(error)
+
+}
+
+
+
+
+};
