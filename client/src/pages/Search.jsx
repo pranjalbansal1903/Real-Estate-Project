328f4e717451c2b151d07c6e54a5d3ca647cda7c
pranjalbansal1903 <pranjalbansal66@gmail.com>
2024-08-27 00:04:02 +0530

    create listing items components and show listing

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,10 @@
 
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 export default function Search() {
 const navigate = useNavigate();
+
     const[sidebardata, setSidebardata] = useState({
 
 searchTerm: '',
@@ -73,7 +75,7 @@ fetchListings();
     }, [location.search]);
 
 
-    }
+    
 
     
 
@@ -252,15 +254,44 @@ navigate(`/search?${searchQuery}`);
         </form>
         </div>
         
-        <div>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results</h1>
+
+            <div className='p-7 flex flex-wrap gap-4'>
+            {!loading && listings.length ===0 && (
+
+                <p className='text-xl text-slate-700'>
+                    No listing found!
+
+                </p>
+            
+            ) }
+            {loading && (
+                <p className='text-xl text-slate-700 text-center w-full'>Loading... </p>
+            )}
+
+            
+
+                {!loading && listings && listings.map((listing) =>(
+                    <ListingItem key={listing._id} listing={listing} />
+
+
+                ))
+                
+            }
+            
+            </div>
+
+
+            </div>
         </div>
         
         
         
         
         
-    </div>
-  )
+    
+  );
+}
 
 
